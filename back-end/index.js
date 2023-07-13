e4ec3c18048105f9defb33b90c0bc2bebb1190f2
Sm0ke070 <smokedeveloper070@gmail.com>
2023-07-13 06:06:32 +0300

    add register/postCreate validation

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import mongoose from 'mongoose'
 import checkAuth from './utils/checkAuth.js'
-import {registerValidation} from './validations/auth.js'
+import {loginValidation, registerValidation} from './validations.js'
 import * as UserController from './controllers/UserController.js'
 
 
--- back-end/validations/auth.js
+++
@@ -1,9 +0,0 @@
-import {body} from 'express-validator'
-
-export const registerValidation = [
-    body('email', 'Неверный формат почты').isEmail(),
-    body('password', 'Минимальная длинна пароля 5 символов').isLength({min: 5}),
-    body('fullName', 'Укажите имя').isLength({min: 3}),
-    body('avatarUrl', 'Некорректная ссылка на аватар').optional().isURL(),
-
-]
